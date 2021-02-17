6e495d3f8a1e11d78f6629f620f6fba3f945441a
ajay-8192 <ajaychowdary3699@gmail.com>
2021-02-17 17:49:55 +0530

    Changed the layout designs

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -4,22 +4,22 @@ import './dashstyle.scss';
 
 
 export const Dashboard = (props) => {
-    const [teams, setteams] = useState([1,2,3,4,5])
+    const [teams, setteams] = useState(["Team1", "Team2", "Team3","Team1", "Team2", "Team3","Team1", "Team2", "Team3"])
 
-    axios({
-        method: 'GET',
-        url:'https://flask-jwt-pro.herokuapp.com/teams',
-        withCredentials: true,
-        headers: {'Content-Type': 'application/json'}
-    }).then(res => {
-        setteams([...res.data])
-        // console.log(res.data);
-    })
+    // axios({
+    //     method: 'GET',
+    //     url:'https://flask-jwt-pro.herokuapp.com/teams',
+    //     withCredentials: true,
+    //     headers: {'Content-Type': 'application/json'}
+    // }).then(res => {
+    //     setteams([...res.data])
+    //     // console.log(res.data);
+    // })
 
     const card = teams.map(team => 
-        <div key={team} className="card" style={{width: 18+'rem'}}>
+        <div key={team} className="card">
             <div className="card-title">
-                <h5>{team.name}</h5>
+                <h5>{team}</h5>
             </div>
         </div>
     )
@@ -28,7 +28,7 @@ export const Dashboard = (props) => {
         <div className="container">
             {/* <Nav /> */}
             <h2>Teams</h2>
-            <div className="content">
+            <div className="cards">
                 {card}
             </div>
             
